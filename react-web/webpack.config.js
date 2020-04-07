ca5eef115f66732d6b74d4a462600bc64d251cb8
Lachlan Tickell <lachlan.tickell@byojet.com>
2020-04-07 19:06:22 -0400

    Updated webpack.config.js

--- react-web/webpack.config.js
+++ react-web/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 //let HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  entry: "./src/javascript/index.tsx",
+  entry: "./src/javascript/Index.tsx",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "./dist"),
